0f778b26caa505ea0db2388255d4b5f6cd26fb99
Nahum Santana <nahumsantanam@gmail.com>
2024-03-24 17:35:52 -0400

    feat: branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-// const site = 'https://lordfitoi.github.io';
-// const base = import.meta.env.MODE != "development" ? 'https://lordfitoi.github.io': "";
+const site = 'https://lordfitoi.github.io/ion-battery-tools/';
+const base = import.meta.env.MODE != "development" ? 'https://lordfitoi.github.io/ion-battery-tools/': "";
 
 export default defineConfig({
-//   site: site,
-//   base: base,
+  site: site,
+  base: base,
   integrations: [tailwind()]
 });
--- src/scripts/PackBuilder.js
+++ src/scripts/PackBuilder.js
@@ -2,35 +2,68 @@ export function sum(array) {
     return array.reduce((acc, val) => acc + val, 0);
 }
 
+class Cell {
+    constructor(amp, slot=-1) {
+        this.amp = amp;
+        this.slot = slot;
+    }
+}
+
+class ParallelPack {
+    constructor(cells=null) {
+        this.cells = cells || [];
+    }
+    addCell(cell) {
+        this.cells.push(cell);
+    }
+    length() {
+        return this.cells.length;
+    }
+    getCapacities() {
+        return this.cells.map(cell => cell.amp);
+    }
+    getTotalCapacity() {
+        return sum(this.getCapacities());
+    }
+}
+
 export class PackBuilder {
-    constructor(batteries, series, parallel) {
-        this.batteries = batteries.sort().slice(-series * parallel);
-        this.meanParallelAmp = sum(this.batteries) / series;
-        this.meanBatteryAmp = this.meanParallelAmp / parallel;
+    constructor(cells, series, parallel) {
         this.parallel = parallel;
         this.series = series;
+        this.cells = this.getCells(cells);
+        this.optimalCells = this.getOptimalCells(this.cells, this.series, this.parallel);
+
+        this.meanParallelAmp = sum(this.optimalCells.map(cell => cell.amp)) / series;
+        this.meanBatteryAmp = this.meanParallelAmp / parallel;
+    }
+    getCells(cells) {
+        return cells.map((amp, index) => new Cell(amp, index + 1));
+    }
+    getOptimalCells(cells, series, parallel) {
+        return cells.sort().slice(-series * parallel);
     }
     getParallelError(parallelPack) {
-        const n = parallelPack.length;
-        const amp = sum(parallelPack);
+        const n = parallelPack.length();
+        const capacity = parallelPack.getTotalCapacity();
 
-        return this.batteries.map((batteryAmp) => {
-            return ((this.meanBatteryAmp * (n+1)) - (amp + batteryAmp)) ** 2;
+        return this.optimalCells.map((cell) => {
+            return ((this.meanBatteryAmp * (n+1)) - (capacity + cell.amp)) ** 2;
         });
     }
     extractBattery(index) {
-        return this.batteries.splice(index, 1)[0];
+        return this.optimalCells.splice(index, 1)[0];
     }
     build() {
         const parallelPacks = [];
 
         for (let i=0; i < this.series; i++) {
-            const parallelPack = [];
+            const parallelPack = new ParallelPack();
 
             for (let i=0; i < this.parallel; i++) {
                 const errors = this.getParallelError(parallelPack);
                 const minErrorIndex = errors.indexOf(Math.min(...errors));
-                parallelPack.push(this.extractBattery(minErrorIndex));
+                parallelPack.addCell(this.extractBattery(minErrorIndex));
             }
 
             parallelPacks.push(parallelPack);
