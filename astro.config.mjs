921ef013f3b0b078b9b41ba137ca6590d41229c1
Nahum Santana <nahumsantanam@gmail.com>
2024-03-24 17:53:54 -0400

    fix: astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 const site = 'https://lordfitoi.github.io/ion-battery-tools/';
-const base = import.meta.env.MODE != "development" ? 'https://lordfitoi.github.io/ion-battery-tools/': "";
+const base = process.env.NODE_ENV != "development" ? '/ion-battery-tools/': "";
 
 export default defineConfig({
   site: site,
